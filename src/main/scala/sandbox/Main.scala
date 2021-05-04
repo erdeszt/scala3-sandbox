db235ffd4d2fddd10409a37b5027be8d27c235c9
erdeszt <erdeszt@gmail.com>
2021-05-04 22:00:53 +0200

    Simplify

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -6,7 +6,7 @@ import scala.deriving._
 
 sealed trait HList
 
-// Why variance?
+// NOTE: Why variance?
 final case class ::[+H, +T <: HList](head: H, tail: T) extends HList:
   def ::[HH](hh: HH): HH :: H :: T = new ::(hh, this)
 
@@ -14,6 +14,7 @@ class HNil extends HList:
   def ::[H](h: H): H :: HNil = new ::(h, this)
 object HNil extends HNil
 
+// NOTE: could use mutable Map or var for more performance perhaps
 class Record[Rec <: HList](storage: Map[String, Any]):
   def apply[Key <: String](using ev: HasField[Rec, Key], keyString: ValueOf[Key]): ev.Out =
     storage.get(keyString.value).get.asInstanceOf[ev.Out]
@@ -32,37 +33,34 @@ final case class Field[Key, Value](key: Key, value: Value)
 
 type ->>[Key, Value] = Field[Key, Value]
 
-extension [Key](key: Key)
-  def ->>[Value](value: Value): Field[Key, Value] =
-    Field(key, value)
-
-@implicitNotFound("Record: ${Rec} doesn't have field: ${Key} or the requested output type is incorrect")
-trait HasField[Rec, Key <: String]:
-  type Out
-  def getField(value: Rec): Out
-
-object HasField:
-  type Aux[Rec, Key <: String, Output] = HasField[Rec, Key] { type Out = Output }
-  given here[Key <: String, T,  Tail <: HList]: HasField[Key ->> T :: Tail, Key] with
-    type Out = T
-    def getField(rec: Key ->> T :: Tail): T =
-      rec.head.value
-    
-  given there[H, Field <: String, Tail <: HList](using tailHasField: HasField[Tail, Field]): HasField[H :: Tail, Field] with
-    type Out = tailHasField.Out
-    def getField(rec: H :: Tail): tailHasField.Out =
-      tailHasField.getField(rec.tail)
-
-  given fromRecord[Key <: String, T, Rec <: HList](using keyString: ValueOf[Key], ev: HasField.Aux[Rec, Key, T]): HasField[Record[Rec], Key] with
-    type Out = T
-    def getField(rec: Record[Rec]): T =
-      rec[Key]
-
 def field[Key <: String]: FieldBuilder[Key] = new FieldBuilder[Key]
 
 class FieldBuilder[Key <: String]:
   inline def apply[Value](value: Value): Field[Key, Value] =
     Field(constValue[Key], value)
+
+@implicitNotFound("Record: ${Rec} doesn't have field: ${Key} or the requested output type is incorrect")
+trait HasField[Rec, Key <: String]:
+  type Out
+  def apply(value: Rec): Out
+
+object HasField:
+  type Aux[Rec, Key <: String, Output] = HasField[Rec, Key] { type Out = Output }
+  given here[Key <: String, T,  Tail <: HList]: HasField[Key ->> T :: Tail, Key] with
+    type Out = T
+    def apply(rec: Key ->> T :: Tail): T =
+      rec.head.value
+    
+  given there[H, Field <: String, Tail <: HList](using tailHasField: HasField[Tail, Field]): HasField[H :: Tail, Field] with
+    type Out = tailHasField.Out
+    def apply(rec: H :: Tail): tailHasField.Out =
+      tailHasField.apply(rec.tail)
+
+  // NOTE: doesn't work without unifying T with the rest of the types(so using ev.Out instead of T)
+  given fromRecord[Key <: String, T, Rec <: HList](using keyString: ValueOf[Key], ev: HasField.Aux[Rec, Key, T]): HasField[Record[Rec], Key] with
+    type Out = T
+    def apply(rec: Record[Rec]): T =
+      rec[Key]
   
 //inline def productToRec[T](using mir: Mirror.Of[T]) =
 //  inline mir match
@@ -77,14 +75,14 @@ inline def test[T](x: T)(using mir: Mirror.Of[T]): Unit =
   println(s"X: ${x}")
 
 inline def powX[Rec](rec: Rec)(using getX: HasField.Aux[Rec, "x", Int]): Int =
-  getX.getField(rec) * getX.getField(rec)
+  getX(rec) * getX(rec)
 
 @main
 def main(): Unit =
   val onlyY = field["y"](true) :: HNil
   val onlyStringX = field["x"]("alma") :: HNil
   val test: TestRec = field["x"](2) :: field["y"](false) :: HNil
-  val testX  = summon[HasField[TestRec, "x"]].getField(test)
+  val testX  = summon[HasField[TestRec, "x"]](test)
   val testXX = powX(test)
   val test2: Record[TestRec] = field["x"](2) +: field["y"](false) +: EmptyRecord
   val test2XX = powX(test2)
