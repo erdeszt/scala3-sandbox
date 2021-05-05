df623e41a5ec3eb746b80141d55b528451c769a5
erdeszt <erdeszt@gmail.com>
2021-05-05 12:53:04 +0200

    Replace HList with Tuple, add syntax sugar for HasField

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -4,29 +4,19 @@ import scala.annotation.implicitNotFound
 import scala.compiletime._
 import scala.deriving._
 
-sealed trait HList
-
-// NOTE: Why variance?
-final case class ::[+H, +T <: HList](head: H, tail: T) extends HList:
-  def ::[HH](hh: HH): HH :: H :: T = new ::(hh, this)
-
-class HNil extends HList:
-  def ::[H](h: H): H :: HNil = new ::(h, this)
-object HNil extends HNil
-
 // NOTE: could use mutable Map or var for more performance perhaps
-class Record[Rec <: HList](storage: Map[String, Any]):
+class Record[Rec <: Tuple](storage: Map[String, Any]):
   def apply[Key <: String](using ev: HasField[Rec, Key], keyString: ValueOf[Key]): ev.Out =
     storage.get(keyString.value).get.asInstanceOf[ev.Out]
     
-  def add[Key <: String, Value](field: Field[Key, Value]): Record[Field[Key, Value] :: Rec] =
+  def add[Key <: String, Value](field: Field[Key, Value]): Record[Field[Key, Value] *: Rec] =
     new Record(storage + (field.key.toString -> field.value))
     
-class EmptyRecord extends Record[HNil](Map.empty)
+class EmptyRecord extends Record[EmptyTuple](Map.empty)
 object EmptyRecord extends EmptyRecord
 
 extension [Key <: String, Value](field: Field[Key, Value])
-  def +:[Rec <: HList](record: Record[Rec]): Record[Field[Key, Value] :: Rec] =
+  def +:[Rec <: Tuple](record: Record[Rec]): Record[Field[Key, Value] *: Rec] =
     record.add(field)
 
 final case class Field[Key, Value](key: Key, value: Value)
@@ -46,42 +36,50 @@ trait HasField[Rec, Key <: String]:
 
 object HasField:
   type Aux[Rec, Key <: String, Output] = HasField[Rec, Key] { type Out = Output }
-  given here[Key <: String, T,  Tail <: HList]: HasField[Key ->> T :: Tail, Key] with
+  given here[Key <: String, T,  Tail <: Tuple]: HasField[(Key ->> T) *: Tail, Key] with
     type Out = T
-    def apply(rec: Key ->> T :: Tail): T =
+    def apply(rec: (Key ->> T) *: Tail): T =
       rec.head.value
-    
-  given there[H, Field <: String, Tail <: HList](using tailHasField: HasField[Tail, Field]): HasField[H :: Tail, Field] with
+
+  given there[H, Field <: String, Tail <: Tuple](using tailHasField: HasField[Tail, Field]): HasField[H *: Tail, Field] with
     type Out = tailHasField.Out
-    def apply(rec: H :: Tail): tailHasField.Out =
+    def apply(rec: H *: Tail): tailHasField.Out =
       tailHasField.apply(rec.tail)
 
   // NOTE: doesn't work without unifying T with the rest of the types(so using ev.Out instead of T)
-  given fromRecord[Key <: String, T, Rec <: HList](using keyString: ValueOf[Key], ev: HasField.Aux[Rec, Key, T]): HasField[Record[Rec], Key] with
+  given fromRecord[Key <: String, T, Rec <: Tuple](using keyString: ValueOf[Key], ev: HasField.Aux[Rec, Key, T]): HasField[Record[Rec], Key] with
     type Out = T
     def apply(rec: Record[Rec]): T =
       rec[Key]
-  
-//inline def productToRec[T](using mir: Mirror.Of[T]) =
-//  inline mir match
-//    case prodMir: Mirror.ProductOf[T] =>
-//      prodMir
 
-type TestRec = "x" ->> Int :: "y" ->> Boolean :: HNil
+//inline def productToRecMapper[Names, Types](element: Product)
+  
+inline def productToRec[T](using mir: Mirror.Of[T]) =
+  inline mir match
+    case prodMir: Mirror.ProductOf[T] =>
+      prodMir
+
+type TestRec = ("x" ->> Int) *: ("y" ->> Boolean) *: EmptyTuple
+type TestRec_ = Field["x", Int] *: Field["y", Boolean] *: EmptyTuple
 
 case class Test(x: Int, y: Boolean)
 
 inline def test[T](x: T)(using mir: Mirror.Of[T]): Unit =
   println(s"X: ${x}")
+  
+type %[Rec, Key <: String] = (Rec, Key)
 
-inline def powX[Rec](rec: Rec)(using getX: HasField.Aux[Rec, "x", Int]): Int =
+type :::[F, T] = F match
+  case (r, k) => HasField.Aux[r, k, T]
+
+inline def powX[Rec](rec: Rec)(using getX: Rec % "x" ::: Int): Int =
   getX(rec) * getX(rec)
 
 @main
 def main(): Unit =
-  val onlyY = field["y"](true) :: HNil
-  val onlyStringX = field["x"]("alma") :: HNil
-  val test: TestRec = field["x"](2) :: field["y"](false) :: HNil
+  val onlyY = field["y"](true) *: EmptyTuple
+  val onlyStringX = field["x"]("alma") *: EmptyTuple
+  val test: TestRec = field["x"](2) *: field["y"](false) *: EmptyTuple
   val testX  = summon[HasField[TestRec, "x"]](test)
   val testXX = powX(test)
   val test2: Record[TestRec] = field["x"](2) +: field["y"](false) +: EmptyRecord
