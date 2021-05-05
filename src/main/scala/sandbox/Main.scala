ecae7dca892c74f8dd4b7796d7654f9ff2b72f48
erdeszt <erdeszt@gmail.com>
2021-05-05 15:48:16 +0200

    Map case classes to records for both encoding

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -8,14 +8,14 @@ import scala.deriving._
 class Record[Rec <: Tuple](storage: Map[String, Any]):
   def apply[Key <: String](using ev: HasField[Rec, Key], keyString: ValueOf[Key]): ev.Out =
     storage.get(keyString.value).get.asInstanceOf[ev.Out]
-    
-  def add[Key <: String, Value](field: Field[Key, Value]): Record[Field[Key, Value] *: Rec] =
+
+  def add[Key, Value](field: Field[Key, Value]): Record[Field[Key, Value] *: Rec] =
     new Record(storage + (field.key.toString -> field.value))
-    
+
 class EmptyRecord extends Record[EmptyTuple](Map.empty)
 object EmptyRecord extends EmptyRecord
 
-extension [Key <: String, Value](field: Field[Key, Value])
+extension [Key, Value](field: Field[Key, Value])
   def +:[Rec <: Tuple](record: Record[Rec]): Record[Field[Key, Value] *: Rec] =
     record.add(field)
 
@@ -23,9 +23,9 @@ final case class Field[Key, Value](key: Key, value: Value)
 
 type ->>[Key, Value] = Field[Key, Value]
 
-def field[Key <: String]: FieldBuilder[Key] = new FieldBuilder[Key]
+def field[Key]: FieldBuilder[Key] = new FieldBuilder[Key]
 
-class FieldBuilder[Key <: String]:
+class FieldBuilder[Key]:
   inline def apply[Value](value: Value): Field[Key, Value] =
     Field(constValue[Key], value)
 
@@ -52,12 +52,40 @@ object HasField:
     def apply(rec: Record[Rec]): T =
       rec[Key]
 
-//inline def productToRecMapper[Names, Types](element: Product)
-  
-inline def productToRec[T](using mir: Mirror.Of[T]) =
+type RecordType[Names, Types] <: Tuple = (Names, Types) match
+  case (name *: names, tpe *: types) => Field[name, tpe] *: RecordType[names, types]
+  case _ => EmptyTuple
+
+type RecordTypeAcc[SoFar <: Tuple, Names, Types] <: Tuple = (Names, Types) match
+  case (name *: names, tpe *: types) => RecordTypeAcc[Field[name, tpe] *: SoFar, names, types]
+  case _ => EmptyTuple
+
+// NOTE: How to get rid of the .asInstanceOf calls?
+inline def productToRecMapper[Names, Types](element: Product, index: Int): RecordType[Names, Types] =
+  inline erasedValue[(Names, Types)] match
+    case (_: (name *: names), _: (tpe *: types)) =>
+      val key = constValue[name].toString
+      val value = element.productElement(index).asInstanceOf[tpe]
+      (field[name](value), productToRecMapper[names, types](element, index + 1)).asInstanceOf[RecordType[Names, Types]]
+    case _ => EmptyTuple.asInstanceOf[RecordType[Names, Types]]
+
+transparent inline def productToRec[T](value: T)(using mir: Mirror.Of[T]): Any =
   inline mir match
     case prodMir: Mirror.ProductOf[T] =>
-      prodMir
+      productToRecMapper[prodMir.MirroredElemLabels, prodMir.MirroredElemTypes](value.asInstanceOf[Product], 0)
+      
+inline def productToRecordMapper[Names, Types, R <: Tuple](element: Product, index: Int, rec: Record[R]): Record[RecordTypeAcc[R, Names, Types]] =
+  inline erasedValue[(Names, Types)] match
+    case (_: (name *: names), _: (tpe *: types)) =>
+      val key = constValue[name].toString
+      val value = element.productElement(index).asInstanceOf[tpe]
+      productToRecordMapper[names, types, Field[name, tpe] *: R](element, index + 1, rec.add[name, tpe](field[name](value))).asInstanceOf[Record[RecordTypeAcc[R, Names, Types]]]
+    case _ => rec.asInstanceOf[Record[RecordTypeAcc[R, Names, Types]]]
+
+transparent inline def productToRecord[T](value: T)(using mir: Mirror.Of[T]): Any =
+  inline mir match
+    case prodMir: Mirror.ProductOf[T] =>
+      productToRecMapper[prodMir.MirroredElemLabels, prodMir.MirroredElemTypes](value.asInstanceOf[Product], 0)
 
 type TestRec = ("x" ->> Int) *: ("y" ->> Boolean) *: EmptyTuple
 type TestRec_ = Field["x", Int] *: Field["y", Boolean] *: EmptyTuple
@@ -66,7 +94,7 @@ case class Test(x: Int, y: Boolean)
 
 inline def test[T](x: T)(using mir: Mirror.Of[T]): Unit =
   println(s"X: ${x}")
-  
+
 type %[Rec, Key <: String] = (Rec, Key)
 
 type :::[F, T] = F match
@@ -79,14 +107,20 @@ inline def powX[Rec](rec: Rec)(using getX: Rec % "x" ::: Int): Int =
 def main(): Unit =
   val onlyY = field["y"](true) *: EmptyTuple
   val onlyStringX = field["x"]("alma") *: EmptyTuple
+  val testc = Test(5, false)
   val test: TestRec = field["x"](2) *: field["y"](false) *: EmptyTuple
   val testX  = summon[HasField[TestRec, "x"]](test)
   val testXX = powX(test)
   val test2: Record[TestRec] = field["x"](2) +: field["y"](false) +: EmptyRecord
   val test2XX = powX(test2)
+  val testcr: TestRec = productToRec(testc)
+  val testcR: TestRec = productToRecord(testc)
+  val testcr2 = powX(testcr)
+  val testcR2 = powX(testcR)
 //  val wrong1: Int = powX(onlyStringX) // Compile time error
 //  val wrong2: Int = powX(onlyY) // Compile time error
   println(s"X: ${testX}, XX: ${testXX}, 2XX: ${test2XX}")
+  println(s"CR: ${testcr}, CR2: ${testcr2}, CRR: ${testcR}, CRR2: ${testcR2}")
 //  test[{ val x: Int }](Test(1, false))
   writetomap.Macro.demo
 //  writetomap.Generic.demo
