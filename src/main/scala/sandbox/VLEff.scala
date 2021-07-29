1e6f625120ed9445bdbd317f72234b35010fe4d0
erdeszt <erdeszt@gmail.com>
2021-07-29 21:43:06 +0200

    Implement Functor & Applicative for FreeVL:

--- src/main/scala/sandbox/VLEff.scala
+++ src/main/scala/sandbox/VLEff.scala
@@ -7,11 +7,11 @@ enum CList:
 import CList.*
 
 trait Functor[F[_]]:
-  def map[A, B](fa: F[A])(f: A => B): F[B]
-trait Applicative[F[_]]:
+  def map[A, B](f: A => B)(fa: F[A]): F[B]
+trait Applicative[F[_]] extends Functor[F]:
   // TODO: <*>
   def pure[A](a: A): F[A]
-trait Monad[F[_]] extends Functor[F]:
+trait Monad[F[_]] extends Applicative[F]:
   def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
 type test = CCons[Functor, CCons[Monad, CNil]]
@@ -23,13 +23,38 @@ enum EffectStack[A <: CList, M[_]]:
       stack:  EffectStack[Effects, M]
   ) extends EffectStack[CCons[Effect, Effects], M]
 
-final case class FreeVL[Effects <: CList, A](runFreeVL: [M[_]] => EffectStack[Effects, M] => M[A])
-// trait FreeVL[Effects <: CList, A]:
-//   def runFreeVL[M[_]](stack: EffectStack[Effects, M]): M[A]
+// final case class FreeVL[Effects <: CList, A](runFreeVL: [M[_]] => Monad[M] ?=> EffectStack[Effects, M] => M[A])
+// fmap
+// FreeVL((stack: [M[_]] => Monad[M] ?=> EffectStack[Effects, M]) => summon[Functor[?]].map(f)(fa.runFreeVL(stack)))
+
+trait FreeVL[Effects <: CList, A]:
+  def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[A]
 
 given freeVLFunctor[Effects <: CList]: Functor[[A] =>> FreeVL[Effects, A]] with
-  def map[A, B](fa: FreeVL[Effects, A])(f: A => B): FreeVL[Effects, B] =
-    null.asInstanceOf[FreeVL[Effects, B]]
+  def map[A, B](f: A => B)(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
+    new FreeVL[Effects, B]:
+      def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[B] =
+        summon[Functor[M]].map(f)(fa.runFreeVL(stack))
+
+given freeVLApplicative[Effects <: CList](using
+    functor: Functor[[A] =>> FreeVL[Effects, A]]
+): Applicative[[A] =>> FreeVL[Effects, A]] with
+  def map[A, B](f: A => B)(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
+    functor.map(f)(fa)
+  def pure[A](a: A): FreeVL[Effects, A] =
+    new FreeVL[Effects, A]:
+      def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[A] =
+        summon[Monad[M]].pure(a)
+
+given freeVLMonad[Effects <: CList](using
+    applicative: Applicative[[A] =>> FreeVL[Effects, A]]
+): Monad[[A] =>> FreeVL[Effects, A]] with
+  def map[A, B](f: A => B)(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
+    applicative.map(f)(fa)
+  def pure[A](a: A): FreeVL[Effects, A] =
+    applicative.pure(a)
+  def flatMap[A, B](fa: FreeVL[Effects, A])(f: A => FreeVL[Effects, B]): FreeVL[Effects, B] =
+    ???
 
 @main
 def vleff(): Unit =
