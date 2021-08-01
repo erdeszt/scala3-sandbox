32f59633ed41678a92e4d7bead4f1d988d3ccb45
erdeszt <erdeszt@gmail.com>
2021-08-01 22:45:17 +0200

    WIP, compiles but fails on one of the unsafe casts

--- build.sbt
+++ build.sbt
@@ -9,9 +9,10 @@ lazy val root = project
     version := "0.1.0",
     scalaVersion := dottyVersion,
     libraryDependencies ++= Seq(
+      "org.typelevel" %% "cats-effect" % "3.2.1",
       "org.scalatest" %% "scalatest" % scalatestVersion % Test,
       "org.scalatest" %% "scalatest-funspec" % scalatestVersion % Test,
       "org.scalatest" %% "scalatest-freespec" % scalatestVersion % Test
     ),
-    // scalacOptions ++= Seq("-Ykind-projector"),
+    scalacOptions ++= Seq("-Ykind-projector")
   )
--- src/main/scala/sandbox/VLEff.scala
+++ src/main/scala/sandbox/VLEff.scala
@@ -1,20 +1,17 @@
 package sandbox
 
+import cats.{Applicative, Functor, Monad}
+import cats.effect.IO
+import cats.effect.IOApp
+
 // Based on: https://aaronlevin.ca/post/136494428283/extensible-effect-stacks-in-the-van-laarhoven-free
 
+// Note works with C <: AnyKind as well, might be more flexible(less strict though)
 enum CList:
   case CNil() extends CList
   case CCons[C[_[_]], T <: CList]() extends CList
 
 import CList.*
-
-trait Functor[F[_]]:
-  def map[A, B](f: A => B)(fa: F[A]): F[B]
-trait Applicative[F[_]] extends Functor[F]:
-  // TODO: <*>
-  def pure[A](a: A): F[A]
-trait Monad[F[_]] extends Applicative[F]:
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
 type test = CCons[Functor, CCons[Monad, CNil]]
 
@@ -25,9 +22,11 @@ enum EffectStack[A <: CList, M[_]]:
       stack:  EffectStack[Effects, M]
   ) extends EffectStack[CCons[Effect, Effects], M]
 
+import EffectStack.*
+
 extension [F[_]: Functor, A](fa: F[A])(using functor: Functor[F])
   def fmap[B](f: A => B): F[B] =
-    functor.map(f)(fa)
+    functor.map(fa)(f)
 
 // Unfortunately this doesn't work. Can't access M[_] in the callback and stack has the wrong inferred type
 // TODO: Report dotty issue
@@ -40,32 +39,138 @@ extension [F[_]: Functor, A](fa: F[A])(using functor: Functor[F])
 trait FreeVL[Effects <: CList, A]:
   def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[A]
 
+extension [Effects <: CList, A](free: FreeVL[Effects, A])(using monad: Monad[[T] =>> FreeVL[Effects, T]])
+  def map[B](f: A => B): FreeVL[Effects, B] =
+    monad.map(free)(f)
+
+  def flatMap[B](f: A => FreeVL[Effects, B]): FreeVL[Effects, B] =
+    monad.flatMap(free)(f)
+
 given freeVLFunctor[Effects <: CList]: Functor[[A] =>> FreeVL[Effects, A]] with
-  def map[A, B](f: A => B)(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
+  def map[A, B](fa: FreeVL[Effects, A])(f: A => B): FreeVL[Effects, B] =
     new FreeVL[Effects, B]:
       def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[B] =
-        summon[Functor[M]].map(f)(fa.runFreeVL(stack))
+        summon[Functor[M]].map(fa.runFreeVL(stack))(f)
 
 given freeVLApplicative[Effects <: CList](using
     functor: Functor[[A] =>> FreeVL[Effects, A]]
 ): Applicative[[A] =>> FreeVL[Effects, A]] with
-  def map[A, B](f: A => B)(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
-    functor.map(f)(fa)
   def pure[A](a: A): FreeVL[Effects, A] =
     new FreeVL[Effects, A]:
       def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[A] =
         summon[Monad[M]].pure(a)
+  // TODO
+  def ap[A, B](ff: FreeVL[Effects, A => B])(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
+    ???
 
 given freeVLMonad[Effects <: CList](using
     applicative: Applicative[[A] =>> FreeVL[Effects, A]]
 ): Monad[[A] =>> FreeVL[Effects, A]] with
-  def map[A, B](f: A => B)(fa: FreeVL[Effects, A]): FreeVL[Effects, B] =
-    applicative.map(f)(fa)
   def pure[A](a: A): FreeVL[Effects, A] =
     applicative.pure(a)
   def flatMap[A, B](fa: FreeVL[Effects, A])(f: A => FreeVL[Effects, B]): FreeVL[Effects, B] =
-    ???
+    new FreeVL[Effects, B]:
+      def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[B] =
+        summon[Monad[M]].flatMap(fa.runFreeVL(stack)) { (a: A) =>
+          f(a).runFreeVL(stack)
+        }
+  def tailRecM[A, B](init: A)(fn: A => FreeVL[Effects, Either[A, B]]): FreeVL[Effects, B] =
+    new FreeVL[Effects, B]:
+      def runFreeVL[M[_]: Monad](stack: EffectStack[Effects, M]): M[B] =
+        summon[Monad[M]].flatMap[Either[A, B], B](fn(init).runFreeVL(stack)) {
+          case Left(a)  => tailRecM(a)(fn).runFreeVL(stack)
+          case Right(b) => summon[Monad[M]].pure(b)
+        }
+
+def interpret[M[_]: Monad, Effects <: CList, A](stack: EffectStack[Effects, M])(program: FreeVL[Effects, A]): M[A] =
+  program.runFreeVL(stack)
+
+trait HasEffect[Effects <: CList, Effect[_[_]]]:
+  def getEffect[M[_]](stack: EffectStack[Effects, M]): Effect[M]
+
+given hasEffectHere[Effects <: CList, Effect[_[_]]]: HasEffect[CCons[Effect, Effects], Effect] with
+  def getEffect[M[_]](stack: EffectStack[CCons[Effect, Effects], M]): Effect[M] =
+    stack.asInstanceOf[ConsEffect[Effect, Effects, M]].effect
+
+given hasEffectThere[Effects <: CList, OtherEffect[_[_]], Effect[_[_]]](using
+    there: HasEffect[Effects, Effect]
+): HasEffect[CCons[OtherEffect, Effects], Effect] with
+  def getEffect[M[_]](stack: EffectStack[CCons[OtherEffect, Effects], M]): Effect[M] =
+    there.getEffect(stack.asInstanceOf[ConsEffect[OtherEffect, Effects, M]].stack)
+
+def liftVL[Effects <: CList, Effect[_[_]]]: LiftVLApply[Effects, Effect] =
+  LiftVLApply[Effects, Effect]()
+
+// If N & M are different then this can go very wrong
+class LiftVLApply[Effects <: CList, Effect[_[_]]]():
+  def apply[M[_], A](f: Effect[M] => M[A])(using effect: HasEffect[Effects, Effect]): FreeVL[Effects, A] =
+    new FreeVL[Effects, A]:
+      def runFreeVL[N[_]: Monad](stack: EffectStack[Effects, N]): N[A] =
+        f(effect.getEffect[N](stack).asInstanceOf[Effect[M]]).asInstanceOf[N[A]]
+
+trait Http[M[_]]:
+  def getHttpEff(url:  String): M[Either[String, String]]
+  def postHttpEff(url: String, body: String): M[Either[String, String]]
+
+trait Logging[M[_]]:
+  def logEff(message: String): M[Unit]
+
+trait Random[M[_]]:
+  def getRandEff: M[Int]
+
+def getHttp[Effects <: CList](url: String)(using
+    effect:                        HasEffect[Effects, Http]
+): FreeVL[Effects, Either[String, String]] =
+  liftVL[Effects, Http](eff => eff.getHttpEff(url))
+
+def postHttp[Effects <: CList](url: String, body: String)(using
+    effect:                         HasEffect[Effects, Http]
+): FreeVL[Effects, Either[String, String]] =
+  liftVL[Effects, Http](eff => eff.postHttpEff(url, body))
+
+def log[Effects <: CList](message: String)(using effect: HasEffect[Effects, Logging]): FreeVL[Effects, Unit] =
+  liftVL[Effects, Logging](eff => eff.logEff(message))
+
+def getRand[Effects <: CList](using effect: HasEffect[Effects, Random]): FreeVL[Effects, Unit] =
+  liftVL[Effects, Random](eff => eff.getRandEff)
+
+def repeatReq[Effects <: CList](url: String)(using
+    http:                            HasEffect[Effects, Http],
+    logging:                         HasEffect[Effects, Logging],
+    random:                          HasEffect[Effects, Random]
+): FreeVL[Effects, Unit] =
+  for
+    numRetries <- getRand
+    response <- getHttp(url)
+    _ <- log(s"Random: ${numRetries}")
+    _ <- log(s"Response: ${response}")
+  yield ()
+
+val httpIO = new Http[IO]:
+  def getHttpEff(url: String): IO[Either[String, String]] =
+    IO.pure(Right(s"You called: ${url}, it's a success"))
+  def postHttpEff(url: String, body: String): IO[Either[String, String]] =
+    IO.pure(Left(s"Call to: ${url} failed"))
+
+val logIO = new Logging[IO]:
+  def logEff(message: String): IO[Unit] =
+    IO(println(s"[LOG] ${message}"))
+
+val randomIO = new Random[IO]:
+  def getRandEff: IO[Int] =
+    IO.pure(4)
+
+type MyEffects = CCons[Http, CCons[Logging, CCons[Random, CNil]]]
+
+val ioInterpreter: EffectStack[MyEffects, IO] =
+  ConsEffect(httpIO, ConsEffect(logIO, ConsEffect(randomIO, EmptyEffect[IO]())))
 
 @main
 def vleff(): Unit =
   println("YOYOYO")
+
+object FreeVLApp extends IOApp.Simple:
+  val run = for {
+    _ <- IO(println("YOPPPU"))
+    _ <- interpret[IO, MyEffects, Unit](ioInterpreter)(repeatReq("foobar"))
+  } yield ()
